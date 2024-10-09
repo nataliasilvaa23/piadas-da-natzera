a7eff54efbab688e3b667d577de263fd00bbdb8c
nataliasilvaa23 <163546452+nataliasilvaa23@users.noreply.github.com>
2024-10-09 09:15:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Piadas',
+    'O que é que uma impressora disse para outra ?',
+    'Essa folha é sua ou é impressão minha ?'
+)
+
+criaCartao(
+    'Humor',
+    'Sabe qual é os santos dos celulares ?',
+    'O são sung'
+)
+
+criaCartao(
+    'Piadas',
+    'Por que o pinheiro nao se perde na floresta ?',
+    'Porque ele tem um mapinha'
+)
+
+criaCartao(
+    'Humor',
+    'Por que o pedreiro terminou o namoro ?',
+    'Porque não era concreto'
+)
